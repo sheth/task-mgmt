ece3976b48dcc85fa7496eb601a57fb5c7600998
Dhaval Sheth <dhaval@dhavalsheth.com>
2019-06-29 22:23:18 -0400

    after validation and error handling part

--- src/tasks/pipes/task-status-validation.pipe.ts
+++ src/tasks/pipes/task-status-validation.pipe.ts
@@ -1,11 +1,24 @@
-import { PipeTransform, ArgumentMetadata } from '@nestjs/common';
+import { PipeTransform, ArgumentMetadata, BadRequestException } from '@nestjs/common';
+import { TaskStatus } from '../task.model';
 
 
 export class TaskStatusValidationPipe implements PipeTransform {
-    transform(value: any, metadata: ArgumentMetadata) {
-        console.log('value', value);
-        console.log('metadata', metadata);
+    readonly validStatuses = [
+        TaskStatus.CLOSE,
+        TaskStatus.IN_PROGRESS,
+        TaskStatus.CLOSE
+    ];
 
+    transform(value: any, metadata: ArgumentMetadata) {
+        value = value.toUpperCase();
+        if(!this.isStatusValid(value)) {
+            throw new BadRequestException(`${value} is an invalid status`);
+        }
         return value;
+    }
+
+    private isStatusValid(status: any) {
+        const idx = this.validStatuses.indexOf(status);
+        return idx !== -1; // if idx is one of [0,1,2] then return true
     }
 }
