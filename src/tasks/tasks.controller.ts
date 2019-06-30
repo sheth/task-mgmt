fb3b762bf9fdcf44942ebba65d57c0fa59606c75
Dhaval Sheth <dhaval@dhavalsheth.com>
2019-06-29 22:00:34 -0400

    doing validation pipes

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,8 +1,9 @@
-import { Controller, Get, Req, Request, Param, Post, Body, Delete, Patch, Query } from '@nestjs/common';
+import { Controller, Get, Req, Request, Param, Post, Body, Delete, Patch, Query, UsePipes, ValidationPipe } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task, TaskStatus } from './task.model';
 import { CreateTaskDto } from './dto/create-task-dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter-dto';
+import { TaskStatusValidationPipe } from './pipes/task-status-validation.pipe';
 
 @Controller('//tasks')
 export class TasksController {
@@ -25,6 +26,7 @@ export class TasksController {
     }
 
     @Post()
+    @UsePipes(ValidationPipe)
     createTask(@Body() createTaskDto: CreateTaskDto): Task {
         return this.tasksService.createTask(createTaskDto);
     }
@@ -35,7 +37,7 @@ export class TasksController {
     }
 
     @Patch('/:id/status')
-    updateStatus(@Param('id') id: string, @Body('status') status: TaskStatus): Task {
+    updateStatus(@Param('id') id: string, @Body('status', TaskStatusValidationPipe) status: TaskStatus): Task {
         return this.tasksService.updateStatus(id, status);
     }
 
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { CreateTaskDto } from './dto/create-task-dto';
 import * as uuid from 'uuid/v1';
@@ -10,7 +10,12 @@ export class TasksService {
 
 
     getTaskById(id: string): Task {
-        return this.tasks.find(task => task.id === id);
+        const found = this.tasks.find(task => task.id === id);
+        if(!found) {
+            throw new NotFoundException(`Task with id ${id} not found`);
+        } else {
+            return found;
+        }
     }
 
     createTask(createTaskDto: CreateTaskDto): Task {
@@ -53,7 +58,7 @@ export class TasksService {
     }
 
     updateStatus(id: string, status: TaskStatus): Task {
-        const t: Task = this.tasks.find(task => task.id === id);
+        const t: Task = this.getTaskById(id);
         if(t) {
             // console.log('in updateStatus status=' + status);
             // t.status = TaskStatus[status];
